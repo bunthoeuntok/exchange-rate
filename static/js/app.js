fa26ba84577309bb9b92947520b91e40cb15dcc5
roeunchamnab <41603649+roeunchamnab@users.noreply.github.com>
2018-12-05 23:54:22 +0700

    new

--- static/js/app.js
+++ static/js/app.js
@@ -1,39 +1,38 @@
 $(document).ready(function () {
-    
-    // modal function
-    $('.date').focus(function(){
+// modal function
+    $('.date').focus(function () {
         var eThis = $(this);
         var today = new Date();
         var dd = today.getDate();
-        var mm = today.getMonth()+1; //January is 0!
+        var mm = today.getMonth() + 1; //January is 0!
         var yyyy = today.getFullYear();
-        if(dd<10) {
-            dd = '0'+dd
-        } 
-
-        if(mm<10) {
-            mm = '0'+mm
-        } 
+        if (dd < 10) {
+            dd = '0' + dd
+        }
+        if (mm < 10) {
+            mm = '0' + mm
+        }
         today = yyyy + '-' + mm + '-' + dd;
         eThis.val(today);
     })
     $('.date').dateDropper();
     $('.show-modal').click(function () {
         var modal = $(this).attr('modal-data')
-        $('#'+modal).css({
-            display: 'block'
+        $('#' + modal).css({
+            // display: 'block'
         })
         $('.modal-card').css({
             animation: 'bounceIn .35s ease'
         });
         $('body').append('<div class="modal-background"></div>')
     })
-    $('body').on('click','.modal-background',function(){
+    $('body').on('click', '.modal-background', function () {
         closefunction()
     })
-    $('body').on('click','.cancel',function(){
+    $('body').on('click', '.cancel', function () {
         closefunction()
-    })    
+    })
+
     function closefunction() {
         $('.modal-card').css({
             animation: 'bounceOut .35s ease'
@@ -43,7 +42,8 @@ $(document).ready(function () {
         })
         $('.modal-background').remove()
     }
-    //material js
+
+//material js
     $('.sidenav-hover').sidenav();
     $('.dropdown-trigger').dropdown({
         coverTrigger: false,
@@ -52,42 +52,41 @@ $(document).ready(function () {
         container: true
     });
     $('select').formSelect();
-    // checkbox onchange event for action
-    // checkbox onchange event for action
-    $('body').on('change','.table #check-action',function () {
+// checkbox onchange event for action
+// checkbox onchange event for action
+    $('body').on('change', '.table #check-action', function () {
         $('.check-action').prop("checked", $(this).prop("checked"));
         var item = $('.check-action:checked').map(function () {
             return $(this).val();
         }).get().join();
         items = item.length;
-        if (items > 1){
+        if (items > 1) {
             $('.action').hide();
             $('.actions').show();
             $('#edit').addClass('disabled');
         }
-        if (items == 0){
+        if (items == 0) {
             $('.action').show();
             $('.actions').hide();
         }
 
     })
-    $('body').on('change','.table .check-action',function () {
+    $('body').on('change', '.table .check-action', function () {
         $(this).prop("checked");
         var item = $("input:checkbox.check-action:checked");
         items = item.length;
-        if (items > 0){
+        if (items > 0) {
             $('.action').hide();
             $('.actions').show();
         }
-        if (items == 0){
+        if (items == 0) {
             $('.action').show();
             $('.actions').hide();
-            $('#check-action').prop('checked',false);
+            $('#check-action').prop('checked', false);
         }
-        if (items > 1){
-           $('#edit').addClass('disabled');
-        }
-        else {
+        if (items > 1) {
+            $('#edit').addClass('disabled');
+        } else {
             $('#edit').removeClass('disabled');
         }
     })
@@ -96,10 +95,10 @@ $(document).ready(function () {
         yearRange: 30,
         format: 'yyyy-mm-dd'
     });
-    // $("body").click(function(e){
-    //     if(e.target.className !== "datepicker-modal" && e.target.className !== "input-field")
-    //     {
-    //       $(".datepicker-modal").hide();
-    //     }
-    // });
+// $("body").click(function(e){
+//     if(e.target.className !== "datepicker-modal" && e.target.className !== "input-field")
+//     {
+//       $(".datepicker-modal").hide();
+//     }
+// });
 });
